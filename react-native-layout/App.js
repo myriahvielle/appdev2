1b215cac38bd7b613948ee66e90a97e3f8b8b38f
unknown <myriahvielle619@gmail.com>
2026-03-22 23:24:31 +0800

    Started with Todo

--- react-native-layout/App.js
+++ react-native-layout/App.js
@@ -1,20 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import Login from './src/screens/Login';
+import Signup from './src/screens/Signup';
+import Todo from './src/screens/Todo';
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 30,
+    backgroundColor: '#d3d3d3'
   },
-});
+  headerContainer:{
+    flex: 1,
+    alignItems:'center',
+    justifyContent: 'space-around'
+  },
+  inputContainer:{
+    flex: 1,
+    alignItems:'flex-end',
+    justifyContent: 'flex-end'
+  },
+  image:{
+    width: '200',
+    height: '200'
+  },
+  header:{
+    fontSize:50
+  }
+})
+
+// export default function App() {
+//   const [page, setPage] = useState('login')
+//   return (
+//     <>
+//       {
+//         page == 'login' ? <Login setPage={setPage} styles={styles}/> : <Signup setPage={setPage} styles={styles}/> 
+//       }
+//     </>
+//   );
+// }
+
+export default function App() {
+ 
+  return (
+    <>
+      <Todo/>
+    </>
+  );
+}
