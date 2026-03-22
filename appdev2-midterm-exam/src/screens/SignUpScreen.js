728ec3e43bce7a1719233320122ae26ee69dd6aa
unknown <myriahvielle619@gmail.com>
2026-03-23 07:36:44 +0800

    Step 2: Update Signup Header Image

--- appdev2-midterm-exam/src/screens/SignUpScreen.js
+++ appdev2-midterm-exam/src/screens/SignUpScreen.js
@@ -14,7 +14,7 @@ export default function SignUpScreen() {
       {/* 1. Header Section */}
       <View style={styles.header}>
         <Image
-          source={require("./../../assets/signup.webp")}
+          source={require("./../../assets/Login.webp")}
           style={styles.image}
         />
       </View>
